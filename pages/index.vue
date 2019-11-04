0e347a2675d3b89110149ba13d6a7012e3818d77
Taylor Ivanoff <taylor@frankdigital.com.au>
2019-11-04 20:22:18 +1100

    Init.

---
+++ pages/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container mx-auto">
+    
+    <div class="flex flex-wrap" data-aos="fade-up">
+
+      <div class="w-full m-8">
+        <h1 class="font-bold text-4xl">Projects</h1>
+        <p class="text-xl">Taylor Ivanoff</p> 
+      </div>
+
+      <div class="w-full sm:w-1/2 md:w-1/3 m-8" >
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/img/bug-tracker.png" alt="Sunset in the mountains">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Bug Tracker</div>
+            <p class="text-gray-700 text-base">
+              A minimalist bug tracker that keeps track of reported issues and software bugs for small web development projects
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg" target="_blank" href="https://bug-tracker.taylorivanoff.com/">View Project</a>
+            <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg"  target="_blank" href="https://github.com/taylorivanoff/bug-tracker">GitHub</a>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#laravel</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#saas</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#cloud</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full sm:w-1/2 md:w-1/3 m-8">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/img/beard-tracker.png" alt="Sunset in the mountains">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Beard Tracker</div>
+            <p class="text-gray-700 text-base">
+              Track your beard with daily reminders to take pics, then create magnificent beard timelapses to share with the world
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg"  target="_blank" href="https://beard-tracker.taylorivanoff.com/">View Project</a>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ios</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#swift</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#movember</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full sm:w-1/2 md:w-1/3 m-8">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/img/bio.jpg" alt="Sunset in the mountains">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Biodegradable Design</div>
+            <p class="text-gray-700 text-base">
+              Designing eco-friendly products using PLA and 3D printing to reduce our carbon footprint and make the future a little greener
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg"  target="_blank" href="https://store.taylorivanoff.com/">View Project</a>
+
+            <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg"  target="_blank" href="https://github.com/taylorivanoff/store">GitHub</a>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#biodegradable</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#3d_printing</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#pollution</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full sm:w-1/2 md:w-1/3 m-8">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="https://i.redd.it/7idxa0f56jj31.gif" alt="Sunset in the mountains">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">G-Code Batch Tool</div>
+            <p class="text-gray-700 text-base">
+               Web app that combines gcode files and repeats it X number of times to automate the 3D printing process
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg"   target="_blank" href="https://gcode-batch-tool.taylorivanoff.com/">View Project</a>
+            <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg"  target="_blank" href="https://github.com/taylorivanoff/gcode-batch-tool">GitHub</a>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#gcode</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#automation</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#webapp</span>
+          </div>
+        </div>
+      </div>
+
+
+
+
+  </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
