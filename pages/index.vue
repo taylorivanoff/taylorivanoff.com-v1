25e1043776cdd400b5c5715f5a2108245569d1d2
Taylor <taylor@Taylors-Bigtincan-MacBook-Pro.local>
2020-02-29 16:34:19 +1100

    Updated projects.

--- pages/index.vue
+++ pages/index.vue
@@ -3,42 +3,15 @@
     
     <div class="flex flex-wrap" data-aos="fade-up">
 
-      <div class="w-full py-12">
-        <h1 class="font-bold text-4xl">Projects</h1>
-        <h3 class="text-xl text-gray-900">by Taylor Ivanoff</h3> 
-      </div>
-
-      <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="/img/bio.jpg" alt="Biodegradable Designs">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Biodegradable Designs</div>
-            <p class="text-gray-700 text-base">
-              3D printed eco-friendly products to make the future a little greener
-
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://store.taylorivanoff.com/">View Store</a>
-            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/store">GitHub</a>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#eco</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#ecommerce</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#wordpress</span>
-            
-
-          </div>
-        </div>
-      </section>
+      <div class="w-full py-12"></div>
 
       <section>
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
           <img class="w-full" src="/img/beard-tracker.png" alt="Beard tracker">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Beard Tracker <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right">v1.2</span></div>
+            <div class="font-bold text-xl mb-2">Beard Tracker</div>
             <p class="text-gray-700 text-base">
-              Track your beard with daily reminders to take pics, then create magnificent beard timelapses to share with the world
+              Track your beard with daily reminders to take pics, then create magnificent beard timelapses to share with the world.
             </p>
           </div>
           <div class="px-6 py-4">
@@ -57,10 +30,10 @@
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
           <img class="w-full" src="/img/bug-tracker.png" alt="Bug tracker">
           <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Bug Tracker</div>
+            <div class="font-bold text-xl mb-2">Bug Tracker <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-orange-500">WIP</span></div>
 
             <p class="text-gray-700 text-base">
-              A card-based issue tracker that tracks reported bugs and tasks for small web development projects
+              A card-based issue tracker that tracks reported bugs and tasks for small web development projects.
             </p>
           </div>
           <div class="px-6 py-4">
@@ -77,11 +50,32 @@
 
       <section>
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/img/consulting.png" alt="Appointment Booking">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Appointment Booking <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-orange-500">Demo</span></div>
+            <p class="text-gray-700 text-base">
+              A appointment booking system built for my consulting services, organising meeting times that work for both parties.
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://consulting.taylorivanoff.com/">View Project</a>
+            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/consulting">GitHub</a>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#booking</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#consulting</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
           <img class="w-full" src="/img/gcode.png" alt="Gcode automation tool">
           <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">G-Code Batch Tool</div>
             <p class="text-gray-700 text-base">
-               A tool used to splice .gcode files and automate a 3D printing production line
+               A tool used to splice .gcode files and automate a 3D printing production line, previously utilized for <strong>biodegradable designs</strong>.
             </p>
           </div>
           <div class="px-6 py-4">
@@ -92,6 +86,26 @@
             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#gcode</span>
             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#automation</span>
             <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <img class="w-full" src="/img/bio.jpg" alt="Biodegradable Designs">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Biodegradable Designs <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-red-500">Offline</span></div>
+            <p class="text-gray-700 text-base">
+              3D printed eco-friendly products to make the future a little greener.
+            </p>
+          </div>
+          <div class="px-6 py-4">
+            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/store">GitHub</a>
+          </div>
+          <div class="px-6 py-4">
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#eco</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#ecommerce</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#wordpress</span>
           </div>
         </div>
       </section>
