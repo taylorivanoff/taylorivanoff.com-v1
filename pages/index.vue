eb2cfbb3d12988078fea4791ee4a4394f67406be
Taylor Ivanoff <taylorivanoff@gmail.com>
2020-05-27 15:19:23 +1000

    Added stock scanner.

--- pages/index.vue
+++ pages/index.vue
@@ -1,116 +1,108 @@
 <template>
   <div class="container mx-auto">
-    
     <div class="flex flex-wrap" data-aos="fade-up">
-
       <div class="w-full py-12"></div>
+        <section>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="/img/scanner.png" alt="Stock Scanner">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">Stock Scanner</div>
+              <p class="text-gray-700 text-base">
+              Live ASX stock scanner that integrates data from multiple sources to provide realtime data as to which stocks are opening at higher price and have news for the day.
+              </p>
+            </div>
+            <div class="px-6 py-4">
+              <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://asx-scanner.taylorivanoff.com/">View Project</a>
+            </div>
+            <div class="px-6 py-4">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#stocks</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#realtime</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
+            </div>
+          </div>
+        </section>
 
-      <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="/img/beard-tracker.png" alt="Beard tracker">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Beard Tracker</div>
-            <p class="text-gray-700 text-base">
-              Track your beard with daily reminders to take pics, then create magnificent beard timelapses to share with the world.
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://beard-tracker.taylorivanoff.com/">View Project</a>
-            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/beard-tracker">GitHub</a>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ios</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#swift</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#movember</span>
-          </div>
-        </div>
-      </section>
+        <section>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="/img/bug-tracker.png" alt="Bug tracker">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">Bug Tracker <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-orange-500">WIP</span></div>
 
-      <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="/img/bug-tracker.png" alt="Bug tracker">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Bug Tracker <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-orange-500">WIP</span></div>
+              <p class="text-gray-700 text-base">
+                A card-based issue tracker that tracks reported bugs and tasks for small web development projects.
+              </p>
+            </div>
+            <div class="px-6 py-4">
+              <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2" target="_blank" href="https://bug-tracker.taylorivanoff.com/">View Project</a>
+            </div>
+            <div class="px-6 py-4">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#workflow</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#tasks</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#laravel</span>
+            </div>
+          </div>
+        </section>
 
-            <p class="text-gray-700 text-base">
-              A card-based issue tracker that tracks reported bugs and tasks for small web development projects.
-            </p>
+        <section>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="/img/consulting.png" alt="Appointment Booking">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">Appointment Booking <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-orange-500">Demo</span></div>
+              <p class="text-gray-700 text-base">
+                A appointment booking system built for consulting services, organising meeting times that work for both parties.
+              </p>
+            </div>
+            <div class="px-6 py-4">
+              <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://consulting.taylorivanoff.com/">View Project</a>
+            </div>
+            <div class="px-6 py-4">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#booking</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#consulting</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
+            </div>
           </div>
-          <div class="px-6 py-4">
-            <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2" target="_blank" href="https://bug-tracker.taylorivanoff.com/">View Prototype</a>
-            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/bug-tracker">GitHub</a>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#bugs</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#saas</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#laravel</span>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="/img/consulting.png" alt="Appointment Booking">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Appointment Booking <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-orange-500">Demo</span></div>
-            <p class="text-gray-700 text-base">
-              A appointment booking system built for my consulting services, organising meeting times that work for both parties.
-            </p>
+        <section>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="/img/gcode.png" alt="Gcode automation tool">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">G-Code Tool</div>
+              <p class="text-gray-700 text-base">
+                 A tool used to splice .gcode files and automate a 3D printing production line, previously utilized for <strong>biodegradable designs</strong>.
+              </p>
+            </div>
+            <div class="px-6 py-4">
+              <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"   target="_blank" href="https://gcode-batch-tool.taylorivanoff.com/">View Project</a>
+            </div>
+            <div class="px-6 py-4">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#gcode</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#automation</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
+            </div>
           </div>
-          <div class="px-6 py-4">
-            <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://consulting.taylorivanoff.com/">View Project</a>
-            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/consulting">GitHub</a>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#booking</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#consulting</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
-          </div>
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="/img/gcode.png" alt="Gcode automation tool">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">G-Code Batch Tool</div>
-            <p class="text-gray-700 text-base">
-               A tool used to splice .gcode files and automate a 3D printing production line, previously utilized for <strong>biodegradable designs</strong>.
-            </p>
+        <section>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src="/img/beard-tracker.png" alt="Beard tracker">
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">Beard Tracker</div>
+              <p class="text-gray-700 text-base">
+                A native swift iOS app that locally stores pics of your beard growth and allows you to create  magnificent beard timelapse to share with the world.
+              </p>
+            </div>
+            <div class="px-6 py-4">
+              <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://beard-tracker.taylorivanoff.com/">View Project</a>
+            </div>
+            <div class="px-6 py-4">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#ios</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#swift</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#movember</span>
+            </div>
           </div>
-          <div class="px-6 py-4">
-            <a class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"   target="_blank" href="https://gcode-batch-tool.taylorivanoff.com/">View Project</a>
-            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/gcode-batch-tool">GitHub</a>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#gcode</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#automation</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#laravel</span>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="/img/bio.jpg" alt="Biodegradable Designs">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Biodegradable Designs <span class="inline-block px-4 text-sm rounded ml-2 leading-loose float-right text-red-500">Offline</span></div>
-            <p class="text-gray-700 text-base">
-              3D printed eco-friendly products to make the future a little greener.
-            </p>
-          </div>
-          <div class="px-6 py-4">
-            <a class="inline-block bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-6 rounded shadow-lg mr-2 mb-2"  target="_blank" href="https://github.com/taylorivanoff/store">GitHub</a>
-          </div>
-          <div class="px-6 py-4">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#eco</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#ecommerce</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mb-2">#wordpress</span>
-          </div>
-        </div>
-      </section>
-
-  </div>
+        </section>
+      </div>
   </div>
 </div>
 </template>
